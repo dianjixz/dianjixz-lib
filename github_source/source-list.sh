3831e399bddae4ea6e0974d0d52cdfe2323649b2
dianjixz <18637716021@163.com>
2023-08-05 20:27:25 +0800

    [update]

---
+++ github_source/source-list.sh
@@ -0,0 +1,14 @@
+# git clone -b xyzw6789abcd1234efgh5678ijkl9012mnop --single-branch --branch <branch_name> <GitHub_Repo_URL>
+
+git clone -b dd5118cab0813078dbbfd116f1489c6a03a42cae --single-branch --branch master https://github.com/dianjixz/mongoose.git
+
+git clone -b 1ab8a8e551a5ab668988549779578553e3c3aa1e --single-branch --branch master https://github.com/dianjixz/libhv.git
+
+git clone -b 905357880a73b5e03fb2adb17b7df10eadcd5e4a --single-branch --branch master https://github.com/dianjixz/RtspServer.git
+
+git clone -b b8c034d72a7d879e92ec3cbc2082481c78205cd1 --single-branch --branch master https://github.com/dianjixz/xop.git
+
+git clone -b cf68fddccefafaa4a5b05082819f66d8419f5811 --single-branch --branch my_test https://github.com/dianjixz/gear-lib.git
+
+
+
