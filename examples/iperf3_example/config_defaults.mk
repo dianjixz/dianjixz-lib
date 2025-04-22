ac359e98dfb70486e637741f8b675645012bc5bc
dianjixz <18637716021@163.com>
2025-04-22 17:49:55 +0800

    [update]

---
+++ examples/iperf3_example/config_defaults.mk
@@ -0,0 +1,5 @@
+
+# # CONFIG_COMPONENT1_ENABLED=y
+CONFIG_TOOLCHAIN_PATH="/opt/arm-nia7ux2111v2e-linux-uclibcgnueabihf/usr/bin"
+CONFIG_TOOLCHAIN_PREFIX="arm-nia7ux2111v2e-linux-uclibcgnueabihf-"
+CONFIG_IPERF3_ENABLED=y
