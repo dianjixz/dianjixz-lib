de44644f72a7cf887b8c53d097f45c7d3a414670
dianjixz <18637716021@163.com>
2025-07-24 12:21:17 +0800

    [update]

---
+++ examples/Protothreads_example/config_defaults.mk
@@ -0,0 +1,6 @@
+
+# # CONFIG_COMPONENT1_ENABLED=y
+
+# CONFIG_TOOLCHAIN_PATH="/opt/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/bin"
+# CONFIG_TOOLCHAIN_PREFIX="aarch64-none-linux-gnu-"
+CONFIG_PT_ENABLED=y
---
+++ examples/Protothreads_example/main/src/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "pt.h"
+#include <unistd.h>
+
+static struct pt pt_led;
+static struct pt pt_count;
+static int count = 0;
+
+
+// LED 闪烁protothread
+static int protothread_led(struct pt *pt) {
+    PT_BEGIN(pt);
+    while (1) {
+        printf("LED ON\n");
+        PT_YIELD(pt);
+        PT_WAIT_UNTIL(pt, (count % 500 == 0)); // 模拟500ms延时
+        printf("LED OFF\n");
+        PT_YIELD(pt); // 挂起、等待下一次调度
+        PT_WAIT_UNTIL(pt, (count % 500 == 0)); // 模拟500ms延时
+    }
+    PT_END(pt);
+}
+
+// 计数protothread
+static int protothread_count(struct pt *pt) {
+    static  int old_count = 0;
+    PT_BEGIN(pt);
+    while (1) {
+        printf("nihao\n");
+        PT_YIELD(pt);
+        PT_WAIT_UNTIL(pt, (count % 5000 == 0)); // 模拟计数变化
+        // PT_YIELD(pt); // 挂起、等待下一次调度
+    }
+    PT_END(pt);
+}
+
+int main(void) {
+    PT_INIT(&pt_led);
+    PT_INIT(&pt_count);
+
+    while (1) {
+        usleep(1000);
+        count ++;
+        protothread_led(&pt_led);
+        protothread_count(&pt_count);
+    }
+    return 0;
+}
