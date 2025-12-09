166e6f818198857ac45ba5c81214f7cc42bb4d55
dianjixz <18637716021@163.com>
2025-12-09 09:37:09 +0800

    [update]

---
+++ examples/tinygps/main/src/main.c
@@ -0,0 +1,100 @@
+#include <termios.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/signal.h>
+#include <sys/types.h>
+#include "tinygps.h"
+
+#define BAUDRATE B115200
+#define FALSE    0
+#define TRUE     1
+//
+volatile int STOP = FALSE;
+void signal_handler_IO(int status);
+int wait_flag       = TRUE;
+char devicename[80] = "/dev/ttySTM2", ch;
+int status;
+
+int newdata = 0;
+
+extern unsigned long  _hdop;
+extern unsigned short _numsats;
+bool gps_encode(char c);
+//
+int main(int argc, char *argv[])
+{
+    int fd, res, i;
+    struct termios newtio;
+    struct sigaction saio;
+    char buf[255];
+    //
+    // open the device in non-blocking way (read will return immediately)
+    fd = open(devicename, O_RDWR | O_NOCTTY | O_NONBLOCK);
+    if (fd < 0) {
+        perror(devicename);
+        exit(1);
+    }
+    //
+    // install the serial handler before making the device asynchronous
+    saio.sa_handler = signal_handler_IO;
+    sigemptyset(&saio.sa_mask);  // saio.sa_mask = 0;
+    saio.sa_flags    = 0;
+    saio.sa_restorer = NULL;
+    sigaction(SIGIO, &saio, NULL);
+    //
+    // allow the process to receive SIGIO
+    fcntl(fd, F_SETOWN, getpid());
+    //
+    // make the file descriptor asynchronous
+    fcntl(fd, F_SETFL, FASYNC);
+    //
+    // set new port settings for canonical input processing
+    newtio.c_cflag     = BAUDRATE | CRTSCTS | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag     = IGNPAR;
+    newtio.c_oflag     = 0;
+    newtio.c_lflag     = 0;
+    newtio.c_cc[VMIN]  = 1;
+    newtio.c_cc[VTIME] = 0;
+    tcflush(fd, TCIFLUSH);
+    tcsetattr(fd, TCSANOW, &newtio);
+    //
+    // loop while waiting for input. normally we would do something useful here
+    while (STOP == FALSE) {
+        //
+        // after receiving SIGIO, wait_flag = FALSE, input is available and can be read */
+        if (wait_flag == FALSE)  // if input is available
+        {
+            res = read(fd, buf, 255);
+            if (res > 0) {
+                for (i = 0; i < res; i++)  // for all chars in string
+                {
+                    // printf("%c", buf[i]);
+                    if (gps_encode(buf[i])) newdata = 1;
+                }
+
+                if (newdata) {
+                    float flat, flon;
+                    unsigned long age;
+                    gps_f_get_position(&flat, &flon, &age);
+                    printf("LAT= %f LON= %f SAT=%d PREC=%ld \n", flat, flon, _numsats, _hdop);
+                    newdata = 0;
+                }
+            }
+            wait_flag = TRUE; /* wait for new input */
+        }
+    }
+    close(fd);
+}
+//
+/***************************************************************************
+ * signal handler. sets wait_flag to FALSE, to indicate above loop that     *
+ * characters have been received.                                           *
+ ***************************************************************************/
+//
+void signal_handler_IO(int status)
+{
+    // printf("received SIGIO signal.\n");
+    wait_flag = FALSE;
+}
